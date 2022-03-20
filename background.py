c2d4e24c861c020ac91a2ddb047e8dceae213752
sksandeep4 <40865955+sksandeep4@users.noreply.github.com>
2022-03-20 18:35:53 +0530

    Update background.py
    Removed unwanted functions for drawing roads and included it into the constructor itself

--- background.py
+++ background.py
@@ -3,17 +3,14 @@ from turtle import Turtle
 
 class Background:
     def __init__(self):
-        pass
-
-    def drawroads(self):
+        
         road1 = Turtle("road1")
         road2 = Turtle("road2")
         road3 = Turtle("road3")
         road1.color("grey")
         road2.color("grey")
         road3.color("grey")
-
-    def drawwhites(self):
+        
         for m in range(0,3):
             for n in range(0, 5):
                 i = Turtle("white")
